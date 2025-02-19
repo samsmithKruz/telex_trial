9a746f0abc64f4dfc8ef6ecca565d7f94ab0d1ea
Smith <samspike46@gmail.com>
2025-02-19 05:08:14 -0500

    patch: updated backdateorder in model

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -69,6 +69,6 @@ class Order extends Model
     public function backDateOrder($order_id)
     {
         $this->db->query("UPDATE orders SET created_at=CURDATE() - INTERVAL 1 DAY WHERE order_id=:id")->bind(":id", $order_id)->execute();
-        return $this->db->rowCount() ? $this->db->lastInsertId() : null;
+        return $this->db->rowCount()  ?: null;
     }
 }
