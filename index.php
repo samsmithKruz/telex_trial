aaec6e4a77e5ec5ed4638013fecb052acd55d202
Smith <samspike46@gmail.com>
2025-02-19 10:29:50 -0500

    update: Added cors from index.php

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
-<?php 
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
 error_reporting(E_ALL); // Report all errors and warnings
 ini_set('display_errors', 1); // Display errors on the screen
 ini_set('display_startup_errors', 1); // Show startup errors
 
 require_once __DIR__ . '/vendor/autoload.php';
+
 use Dotenv\Dotenv;
 use App\Lib\Helpers;
 use App\Lib\Core;
@@ -20,7 +25,7 @@ foreach ($_ENV as $key => $value) {
     putenv("$key=$value");
 }
 define("APP", getenv('APP_NAME') ?: "App");
-define('DOMAIN', getenv("APP_URL")?:'http://localhost:5500');
+define('DOMAIN', getenv("APP_URL") ?: 'http://localhost:5500');
 $_SESSION[APP] = $_SESSION[APP] ?? new stdClass;
 
 require_once __DIR__ . '/src/Lib/functions.php';
